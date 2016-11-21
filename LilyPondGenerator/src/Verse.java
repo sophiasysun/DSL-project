33626283d0da3d88982e1f1955dd0ad5637cc7ee
sophiasysun <sophia.s.sun@gmail.com>
2016-11-20 21:54:52 -0800

    Update Verse.java

--- LilyPondGenerator/src/Verse.java
+++ LilyPondGenerator/src/Verse.java
@@ -7,34 +7,48 @@ public class Verse {
 	String name;
 	ArrayList<String []> chords = new ArrayList<String []>();
 
+	/**
+	 * Verse constructor
+	 * @return
+	 */
 	public Verse (String name, ArrayList<String []> chords) {
 		this.name = name;
 		this.chords = chords;
-
 	}
 
+	/**
+	 * Return the name of a verse
+	 * @return
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * Return the array of chords in a verse
+	 * @return
+	 */
 	public ArrayList<String[]> getChords() {
 		return this.chords;
 	}
 
-
-	public String toString (String name) {
+	/**
+	 * Print the chords in one verse 
+	 * @return
+	 */
+	public String toString () {
 		String s ="";
 		String finalStr ="";
 
+		// for all the chords in this verse
 		for (int j=0; j<chords.size(); j++){
+			
+			// print each element of the chord 
 			for (int i=0; i<chords.get(j).length; i++){
-
-				s+=chords.get(j)[i]+ " ";
+				s+=chords.get(j)[i]+ "|";
 			}
-			System.out.println(s);
-			System.out.println();
-			finalStr+=s;
-			s="";
+			finalStr+=s+ "\n ";
+			s ="";
 		}
 		return finalStr;
 	}
